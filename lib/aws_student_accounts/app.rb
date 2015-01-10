f94c01904fab3d16742d866bd321a30289bb791d
Dr Nic Williams <drnicwilliams@gmail.com>
2015-01-10 10:21:52 -0800

    load up fog file

--- lib/aws_student_accounts/app.rb
+++ lib/aws_student_accounts/app.rb
@@ -1,21 +1,49 @@
 require "thor"
+require "yaml"
 
 class AwsStudentAccounts::App < Thor
   include Thor::Actions
 
+  def self.common_options
+    method_option :fog_file, type: :string, aliases: "-C", desc: "Path to YAML file of fog credentials"
+  end
+
   desc "verify-credentials", "Verify AWS credentials"
+  common_options
   def verify_credentials
+    load_and_verify_options
   end
 
   desc "create-students", "Create a student IAM account for all AWS accounts"
+  common_options
   def create_students
+    load_and_verify_options
   end
 
   desc "delete-students", "Delete temporary student IAM accounts"
+  common_options
   def delete_students
+    load_and_verify_options
   end
 
   desc "clean-accounts", "Clean out all VMs, disk, elastic IPs, AMIs, VPCs from student accounts"
+  common_options
   def clean_accounts
+    load_and_verify_options
   end
+
+  private
+  def load_and_verify_options
+    @fog_file = options["fog_file"]
+    unless File.exists?(@fog_file)
+      say "File #{@fog_file} does not exist", :red
+      exit 1
+    end
+    @fog_credentials = YAML.load_file(@fog_file)
+    if !@fog_credentials.is_a?(Hash) || @fog_credentials.first.is_a?(Hash)
+      say "File #{@fog_file} does not match a .fog format (Hash of Hashes)", :red
+      exit 1
+    end
+  end
+
 end
